b10e593162ae862011e6ee700205457ddb068b58
unknown <nicolasgonzalezmoller@gmail.com>
2024-08-25 23:57:31 -0400

    aws

--- database/db.sql
+++ database/db.sql
@@ -1,16 +1,4 @@
-CREATE TABLE users (
-
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(225) NOT NULL, 
-    email VARCHAR(225) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-
-);
-
-INSERT INTO users (name, email) 
-    VALUES ('nico gonzalez', 'nico@gmail.com'),  ('Cami', 'cami@gmail.com');
-
-SELECT * FROM users;CREATE TABLE users (
+CREATE TABLE userss(
 
     id SERIAL PRIMARY KEY,
     name VARCHAR(225) NOT NULL, 
